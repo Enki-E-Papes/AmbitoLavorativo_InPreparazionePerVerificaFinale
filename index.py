1a7a166ac24f223e64806a868cfba9878ed7125c
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2023-04-18 10:57:16 +0000

    c

--- index.py
+++ index.py
@@ -9,16 +9,20 @@ app = Flask(__name__)   #variabile che identifica il sito web
 @app.route('/', methods=['GET'])  #sono tutte le possibili richieste del utente
 def esPrincipale():
     df = pd.read_excel('https://github.com/wtitze/3E/blob/main/BikeStores.xls?raw=true', sheet_name='customers')
-    #input Html
-    Se = df[(df.first_name == nome) & (df.last_name == cognome)]
-    return render_template("index1.html", Se = Se.to_html())
+    return render_template("index1.html")
 
 @app.route('/es1', methods=['GET'])  #sono tutte le possibili richieste del utente
 def es1():
+    Se = df[(df.first_name == nome) & (df.last_name == cognome)]
+    return render_template("index2.html", Se = Se.to_html())
+
+@app.route('/es2', methods=['GET'])  #sono tutte le possibili richieste del utente
+def es2():
     df = pd.read_excel('https://github.com/wtitze/3E/blob/main/BikeStores.xls?raw=true', sheet_name='customers')
     #input Html
     Se = df[(df.first_name == nome) & (df.last_name == cognome)]
-    return render_template("index1.html", Se = Se.to_html())
+    return render_template("index2.html", Se = Se.to_html())
+
 
 
 
